7ec922eca0d0c8a5959e74f1911dfb837c9e506b
Patrick Heyer <9436503+PatTheMav@users.noreply.github.com>
2024-02-28 22:08:51 +0000

    clang-format@17: update 17.0.3_1 bottle.

--- Formula/clang-format@17.rb
+++ Formula/clang-format@17.rb
@@ -16,9 +16,10 @@ class ClangFormatAT17 < Formula
   end
 
   bottle do
-    root_url "https://github.com/obsproject/homebrew-tools/releases/download/clang-format@17-17.0.3"
-    sha256 cellar: :any_skip_relocation, ventura:      "afc4f7003d0c4e98be6bdbfa70a644b30e250ff764e560d2055b545afefdce8f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "daaf7942d750c0133e703573cc137b72c5dce303d1db84e2e978a2c92d3bad80"
+    root_url "https://github.com/obsproject/homebrew-tools/releases/download/clang-format@17-17.0.3_1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "091143e26b4f684fffe051948fcc57697a83c243206cd6aeca7356e5cc017c72"
+    sha256 cellar: :any_skip_relocation, ventura:      "5bc3aeaeeaf5f0e348d0fd061934cd5be2a3651b72ffb4bdd601afcdb85a5a34"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fd0babec17b8392e12b9f7fc7b6ae131693545689f2d80643bddd3e8373dae89"
   end
 
   depends_on "cmake" => :build
